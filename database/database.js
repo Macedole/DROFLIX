ad60fc7d6b09c5d9a2cdeddb1dfcd74a2122f1cb
Vanessa Ike <vanessaike7@gmail.com>
2022-10-06 23:11:58 -0300

    alteração da tela inicial e da barra de admin

--- database/database.js
+++ database/database.js
@@ -1,11 +1,12 @@
-const mysql = require('mysql2');
+const mysql = require("mysql2");
 
 let pool = mysql.createPool({
-    connectionLimit: 10,
-    host     : 'localhost',
-    database : 'db_droflix',
-    user     : 'root',
-    password : 'ph27092002'
+  connectionLimit: 10,
+  host: "leandromacedo.mysql.dbaas.com.br",
+  port: "3306",
+  user: "leandromacedo",
+  password: "gmJk3UX5W#",
+  database: "leandromacedo",
 });
 
 module.exports = pool;

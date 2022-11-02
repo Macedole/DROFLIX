b0d42f44d35cf4d1dbaa9c61bf4006ea9d5cb9ac
Paulo Leal <devpauloleal@gmail.com>
2022-11-02 16:30:43 -0300

    Desenvolvimento da recuperação de senha do cliente

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,17 +1,116 @@
+const modelCliente = require("../models/Cliente.js");
+const disparaEmail = require("../public/assets/utils/disparaEmail.js");
+
+const bcrypt = require("bcryptjs");
+
 class AuthController {
   getLogin(req, res) {
     res.render("auth/login", {
       paginaTitulo: "Login",
-      isLoggedIn: false,
+      isLoggedIn: false
     });
   };
 
   getCadastro(req, res) {
     res.render("auth/cadastro", {
       paginaTitulo: "Criar conta",
-      isLoggedIn: false,
+      isLoggedIn: false
     });
   };
+
+  renderRecuperacao(req, res) {
+    res.render("auth/recuperar-senha", {
+      paginaTitulo: "Recuperação de senha",
+      isLoggedIn: false
+    });
+  }
+
+  async disparaEmail(req, res) {
+    const { email } = req.body;
+
+    const cliente = await modelCliente.getCustomer({ campo: 'email', valor: email });
+
+    if(cliente.length == 0) {
+      return res.json({ erro: true, mensagem: 'E-mail não localizado em nossa base de dados!' });
+    }
+
+    let codigo = '';
+
+    for(let i = 0; i < 6; i++) {
+      codigo += Math.floor(Math.random() * 10);
+    }
+
+    const codigoVerificacao = await modelCliente.storeCodigoVerificacao({ 
+      tabela: 'clientes', 
+      id: cliente[0].id_Cliente,
+      codigo
+    });
+
+    if(codigoVerificacao.affectedRows > 0) {
+      const texto = `
+        <h1>Olá ${cliente[0].nome}</h1>
+        <p>Idenficamos que você solicitou a recuperação de sua senha.</p>
+        <p>Para redefinir sua senha, informe o seguinte código na interface de redefinição de senha.</p>
+        <h2><span style="color: green">${codigo}</span></h2>
+        <p>Caso você não tenha solicitado a recuperação de senha, por gentileza, desconsidere esse e-mail.</p>
+        <p>Atenciosamente, </p>
+        <p>Suporte Droflix </p>
+      `;
+
+      const disparo = await disparaEmail({
+        email: cliente[0].email,
+        assunto: 'Recuperação de Senha',
+        texto
+      });
+
+      if (disparo) {
+        return res.json({ erro: false, mensagem: `E-mail enviado com sucesso para ${cliente[0].email}` });
+      } else {
+        return res.json({ erro: true, mensagem: 'Houve um erro ao enviar o e-mail!' });
+      }
+    } else {
+      return res.json({ erro: true, mensagem: 'Houve um erro ao enviar o e-mail!' });
+    }
+  }
+
+  async verificaCodVerificacao (req, res) {
+    const { email, codigo } = req.body;
+
+    const cliente = await modelCliente.getCustomer({ campo: 'email', valor: email });
+
+    if(cliente.length == 0) {
+      return res.json({ erro: true, mensagem: 'E-mail não localizado em nossa base de dados!' });
+    }
+
+    if(cliente[0].cod_verificacao !== codigo) {
+      return res.json({ erro: true, mensagem: 'O código de verificação está incorreto!' });
+    }
+
+    return res.json({ erro: false });
+  }
+
+  async updateSenha (req, res) {
+    const { email, senha } = req.body;
+
+    const cliente = await modelCliente.getCustomer({ campo: 'email', valor: email });
+
+    if(cliente.length == 0) {
+      return res.json({ erro: true, mensagem: 'E-mail não localizado em nossa base de dados!' });
+    }
+
+    const salt = bcrypt.genSaltSync(10);
+    const hash = bcrypt.hashSync(senha, salt);
+
+    const alteracao = await modelCliente.updateSenha( { 
+      tabela: 'clientes', 
+      id: cliente[0].id_Cliente,
+      senha: hash
+    });
+
+    if(alteracao.affectedRows > 0) {
+      return res.json({ erro: false, mensagem: 'Senha atualizada com sucesso!' });
+    }
+  }
 }
 
 module.exports = new AuthController;
--- routes/auth.js
+++ routes/auth.js
@@ -5,5 +5,9 @@ const authController = require("../controllers/auth");
 
 router.get("/login", authController.getLogin);
 router.get("/cadastro", authController.getCadastro);
+router.get("/recuperacao-senha", authController.renderRecuperacao);
+router.post("/recuperacao-senha", authController.disparaEmail);
+router.post("/recuperacao-senha/verificacao", authController.verificaCodVerificacao);
+router.post("/alteracao-senha", authController.updateSenha);
 
 module.exports = router;

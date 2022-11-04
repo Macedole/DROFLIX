b5a149213421140d96efb8cfa885e73baf5d5171
Leandro Macedo <leandromacedo976@gmail.com>
2022-11-03 21:36:15 -0300

    alterando o auth

--- routes/servico.js
+++ routes/servico.js
@@ -2,12 +2,13 @@ const express = require("express");
 const router = express.Router();
 const servicoController = require("../controllers/servicoController");
 const shopController = require("../controllers/shop.js");
-const auth = require("../middlewares/authFuncionario");
+const authFuncionario = require("../middlewares/authFuncionario");
+const authCliente = require("../middlewares/authCliente");
 
 
-router.get("/servico", auth, servicoController.renderCadastroServico);
-router.post("/servico", auth, servicoController.storeServico);
-router.get("/servicos/:id", auth, servicoController.getServicos);
-router.post("/servico/agendamento", auth, servicoController.storeAgendamento);
+router.get("/servico", authFuncionario, servicoController.renderCadastroServico);
+router.post("/servico", authFuncionario, servicoController.storeServico);
+router.get("/servicos/:id", authFuncionario, servicoController.getServicos);
+router.post("/servico/agendamento", authCliente, servicoController.storeAgendamento);
 router.get("/servico/:servicoId", shopController.getServico);
 module.exports = router;
